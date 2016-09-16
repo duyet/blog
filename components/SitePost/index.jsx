e404f3820236cc020b79c020c5b2aa39d0d7e856
Simon Reinsperger <reinsperger.smn@gmail.com>
2016-09-16 11:02:43 +0200

    Fixes Twitter Link in SitePost component

--- components/SitePost/index.jsx
+++ components/SitePost/index.jsx
@@ -36,7 +36,7 @@ class SitePost extends React.Component {
                   <hr></hr>
                   <p>
                     { config.siteDescr }
-                    <a href={ config.twitter }>
+                    <a href={ config.siteTwitterUrl }>
                       <br></br> <strong>{ config.siteAuthor }</strong> on Twitter</a>
                   </p>
                 </div>
