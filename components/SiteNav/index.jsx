7762d68aef90aa9229f33659f4d8fa0ee626ae6d
Simon Reinsperger <reinsperger.smn@gmail.com>
2017-03-08 17:30:14 +0100

    refactor: fixes linting errors components/SiteNav/index.jsx

--- components/SiteNav/index.jsx
+++ components/SiteNav/index.jsx
@@ -1,34 +1,29 @@
-import React from 'react'
-import { RouteHandler, Link } from 'react-router'
-import { prefixLink } from 'gatsby-helpers'
-import './style.css'
+import React from 'react';
+import { Link } from 'react-router';
+import { prefixLink } from 'gatsby-helpers';
+import './style.css';
 
 class SiteNav extends React.Component {
-    render() {
-        const {location} = this.props
-        return (
-            <nav className='blog-nav'>
-              <ul>
-                <li>
-                  <Link to={ prefixLink('/')} activeClassName="current" onlyActiveOnIndex> Articles
-                  </Link>
-                </li>
-                <li>
-                  <Link to={ prefixLink('/about/')} activeClassName="current"> About me
-                  </Link>
-                </li>
-                <li>
-                  <Link to={ prefixLink('/contact/')} activeClassName="current"> Contact me
-                  </Link>
-                </li>
-              </ul>
-            </nav>
-            );
-    }
+  render() {
+    return (
+      <nav className="blog-nav">
+        <ul>
+          <li>
+            <Link to={prefixLink('/')} activeClassName="current" onlyActiveOnIndex> Articles
+            </Link>
+          </li>
+          <li>
+            <Link to={prefixLink('/about/')} activeClassName="current"> About me
+            </Link>
+          </li>
+          <li>
+            <Link to={prefixLink('/contact/')} activeClassName="current"> Contact me
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
 }
 
-SiteNav.propTypes = {
-    location: React.PropTypes.object,
-}
-
-export default SiteNav
+export default SiteNav;
