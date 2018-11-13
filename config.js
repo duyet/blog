42e676ef63aef36bbf8a2fe17e5158948a5a59df
alxshelepenok <alxshelepenok@gmail.com>
2018-11-13 20:35:36 +0300

    Change slugs

--- config.js
+++ config.js
@@ -15,11 +15,11 @@ module.exports = {
     },
     {
       label: 'About me',
-      path: '/about'
+      path: '/pages/about'
     },
     {
       label: 'Contact me',
-      path: '/contacts'
+      path: '/pages/contacts'
     }
   ],
   author: {
