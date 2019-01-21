1a7deacd3f075d64ddf9456e26aa97000d75597f
alxshelepenok <alxshelepenok@gmail.com>
2019-01-21 15:32:18 +0300

    Fix Netlify CMS templates

--- src/cms/preview-templates/post-preview.js
+++ src/cms/preview-templates/post-preview.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 const PostPreview = ({ entry, widgetFor }) => {
-  const body = widgetFor(['body']);
+  const body = widgetFor('body');
   const title = entry.getIn(['data', 'title']);
 
   return (
     <div className="post">
       <h1 className="post__title">{title}</h1>
-      <div className="post__body" dangerouslySetInnerHTML={{ __html: body }} />
+      <div className="post__body">{body}</div>
     </div>
   );
 };
