5d2fbb3f58f82897eaa0f4f6473200a9ba659976
Victor Zhou <vzhou842@gmail.com>
2019-02-09 17:50:54 -0500

    Add lang="en" to <html>

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -5,6 +5,7 @@ import styles from './Layout.module.scss';
 const Layout = ({ children, title, description }) => (
   <div className={styles.layout}>
     <Helmet>
+      <html lang="en" />
       <title>{title}</title>
       <meta name="description" content={description} />
     </Helmet>
