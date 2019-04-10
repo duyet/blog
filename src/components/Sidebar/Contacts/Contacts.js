1b863ade9e9e2a1e2ba5fa958b289311bbb30424
Victor Zhou <vzhou842@gmail.com>
2019-04-10 16:23:17 -0400

    Add Flow typing for Sidebar/Contacts

--- src/components/Sidebar/Contacts/Contacts.js
+++ src/components/Sidebar/Contacts/Contacts.js
@@ -1,9 +1,16 @@
+// @flow
 import React from 'react';
 import { getContactHref, getIcon } from '../../../utils';
 import Icon from '../../Icon';
 import styles from './Contacts.module.scss';
 
-const Contacts = ({ contacts }) => (
+type Props = {|
+  +contacts: {
+    [string]: string,
+  },
+|};
+
+const Contacts = ({ contacts }: Props) => (
   <div className={styles['contacts']}>
     <ul className={styles['contacts__list']}>
       {Object.keys(contacts).map((name) => (
