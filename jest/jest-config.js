89192b70aeedd33c6851c40aa11f5607b9b454c1
alxshelepenok <alxshelepenok@gmail.com>
2019-05-09 00:56:56 +0300

    fix: update path

--- jest/jest-config.js
+++ jest/jest-config.js
@@ -3,12 +3,12 @@
 module.exports = {
   rootDir: '../',
   transform: {
-    '^.+\\.js?$': '<rootDir>/tests/jest-preprocess.js'
+    '^.+\\.js?$': '<rootDir>/jest/jest-preprocess.js'
   },
   testMatch: ['**/__tests__/**/*.js', '**/?(*.)test.js'],
   moduleNameMapper: {
     '.+\\.(css|styl|less|sass|scss)$': 'identity-obj-proxy',
-    '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/tests/__mocks__/fileMock.js'
+    '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/jest/__mocks__/fileMock.js'
   },
   testPathIgnorePatterns: ['node_modules', '.cache', 'public'],
   transformIgnorePatterns: [
@@ -18,5 +18,5 @@ module.exports = {
     __PATH_PREFIX__: ''
   },
   testURL: 'http://localhost/',
-  setupFiles: ['<rootDir>/tests/loadershim.js']
+  setupFiles: ['<rootDir>/jest/loadershim.js']
 };
