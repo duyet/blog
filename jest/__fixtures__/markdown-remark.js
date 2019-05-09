5b3dcd9b8316e05df557cec536f1a9022cc9c4a1
alxshelepenok <alxshelepenok@gmail.com>
2019-05-09 16:37:54 +0300

    test: add fixtures

---
+++ jest/__fixtures__/markdown-remark.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = {
+  markdownRemark: {
+    html: '<p>test</p>',
+    fields: {
+      tagSlugs: [
+        '/test_0',
+        '/test_1'
+      ]
+    },
+    frontmatter: {
+      date: '2016-09-01',
+      description: 'test',
+      title: 'test',
+      tags: [
+        'test_0',
+        'test_1'
+      ]
+    }
+  }
+};
