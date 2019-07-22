3b6b3649d2fe25fd398f353def670b28204ef2f6
Victor Zhou <vzhou842@gmail.com>
2019-07-22 16:03:42 -0400

    Update Contacts.js

--- src/components/Sidebar/Contacts/Contacts.js
+++ src/components/Sidebar/Contacts/Contacts.js
@@ -13,8 +13,7 @@ type Props = {
 const Contacts = ({ contacts }: Props) => (
   <div className={styles['contacts']}>
     <ul className={styles['contacts__list']}>
-      {Object.keys(contacts).map((name) =>
-      !contacts[name] ? null : (
+      {Object.keys(contacts).map((name) => !contacts[name] ? null : (
         <li className={styles['contacts__list-item']} key={name}>
           <a
             className={styles['contacts__list-item-link']}
