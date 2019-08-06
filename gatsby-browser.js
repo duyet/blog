f314302565da7d058b23f73fc5894a7307fce6a9
Duyet Le <lvduyet@fossil.com>
2019-08-06 23:35:26 +0700

    Update font size, update prism theme

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,5 @@
 'use strict';
 
 require('./src/assets/scss/init.scss');
-require('./static/css/prismjs/theme.min.css');
+// require('./static/css/prismjs/theme.min.css');
+require('./static/css/prismjs/prism-tomorrow.css');
