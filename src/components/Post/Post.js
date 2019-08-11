abbae30a985314bfd937c094eb1897f00bf291c5
Duyet Le <lvduyet@fossil.com>
2019-08-11 10:17:00 +0700

    Update layout

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -31,7 +31,6 @@ const Post = ({ post }: Props) => {
       <div className={styles['post__footer']}>
         <Meta date={date} />
         {tags && tagSlugs && <Tags tags={tags} tagSlugs={tagSlugs} />}
-        <Author />
       </div>
 
       <div className={styles['post__comments']}>
