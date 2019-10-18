b7333f621a289502bb2cf6af0a72f8e57f9276c9
Van-Duyet Le <5009534+duyetdev@users.noreply.github.com>
2019-10-18 10:21:14 +0700

    Update gatsby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,13 +4,3 @@
 'use strict';
 
 require('./src/assets/scss/init.scss');
-
-export const onServiceWorkerUpdateReady = () => {
-  const answer = window.confirm(
-    'This application has been updated. '
-        + 'Reload to display the latest version?'
-  );
-  if (answer === true) {
-    window.location.reload();
-  }
-};
