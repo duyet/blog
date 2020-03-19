d766e45969d545411cf2adccd58ac201280ea1f3
Duyet Le <lvduyet@fossil.com>
2020-03-19 13:38:12 +0700

    Disable gatsby-plugin-netlify-cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -150,12 +150,12 @@ module.exports = {
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
     'gatsby-plugin-netlify',
-    {
-      resolve: 'gatsby-plugin-netlify-cms',
-      options: {
-        modulePath: `${__dirname}/src/cms/index.js`,
-      }
-    },
+    // {
+    //   resolve: 'gatsby-plugin-netlify-cms',
+    //   options: {
+    //     modulePath: `${__dirname}/src/cms/index.js`,
+    //   }
+    // },
     {
       resolve: 'gatsby-plugin-google-gtag',
       options: {
