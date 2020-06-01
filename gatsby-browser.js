89244a8b6c9e1f3688b5990d6dd5100b62090479
Duyet Le <lvduyet@fossil.com>
2020-06-01 21:28:48 +0700

    Prompt to reload page wehn service worker updated

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,3 +4,13 @@
 'use strict';
 
 require('./src/assets/scss/init.scss');
+
+export const onServiceWorkerUpdateReady = () => {
+  const answer = window.confirm(
+    'This blog has been updated. '
+      + 'Reload to display the latest version?'
+  );
+  if (answer === true) {
+    window.location.reload();
+  }
+};
