1c1046c93580d265da18d3edb1729b4b8505a2ed
duyet <duyet@users.noreply.github.com>
2020-12-26 11:45:49 +0000

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-730fd5125b3bd4aae8d1.js"
+    "url": "webpack-runtime-86b8341af28db1aa1d11.js"
   },
   {
     "url": "styles.3af78d1a2a725c821497.css"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "e04f11abd2fd6b9ec2499b28f797fded"
+    "revision": "7f7f779877b5950bbe27153b1a73cb9e"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-e5cb9e0c77a823b86dc2.js"
