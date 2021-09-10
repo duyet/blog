ae1122848f2574e499f25f4785695ef9da084233
Duyet Le <lvduyet@fossil.com>
2021-09-10 17:28:07 +0700

    feat: add Kofi button

---
+++ src/components/Post/Kofi/Kofi.tsx
@@ -0,0 +1,10 @@
+// @flow strict
+import React from 'react';
+
+const Meta = () => (
+  <a href="https://ko-fi.com/F1F6WR3R" target="_blank">
+    <img height={36} style={{ border: 0, height: 36 }} src="https://cdn.ko-fi.com/cdn/kofi3.png?v=3" alt="Buy Me a Coffee at ko-fi.com" />
+  </a>
+);
+
+export default Meta;
--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -4,7 +4,7 @@ import { Link } from "gatsby";
 
 import Comments from "./Comments";
 import Content from "./Content";
-import Meta from "./Meta";
+import Kofi from "./Kofi";
 import Tags from "./Tags";
 // import SubscribeBox from './SubscribeBox';
 
@@ -15,40 +15,54 @@ import { gtagTrack } from "../../utils";
 import "katex/dist/katex.min.css";
 
 type Props = {
-    post: Node;
+  post: Node;
 };
 
 const Post = ({ post }: Props) => {
-    const [showComment, toggleShowComment] = useState<boolean>(false);
-    const { html } = post;
-    const { tagSlugs, slug } = post.fields;
-    const { tags, title, date, fbCommentUrl } = post.frontmatter;
-    const siteTitle = useSiteMetadata().title;
+  const [showComment, toggleShowComment] = useState<boolean>(false);
+  const { html } = post;
+  const { tagSlugs, slug } = post.fields;
+  const { tags, title, date, fbCommentUrl } = post.frontmatter;
+  const siteTitle = useSiteMetadata().title;
 
-    return (
-        <div className={styles.post}>
-            <Link className={styles["post__homeButton"]} to="/" onClick={() => gtagTrack("HomeLink", "click", "home")}>
-                {siteTitle}
-            </Link>
+  return (
+    <div className={styles.post}>
+      <Link
+        className={styles["post__homeButton"]}
+        to="/"
+        onClick={() => gtagTrack("HomeLink", "click", "home")}
+      >
+        {siteTitle}
+      </Link>
 
-            <div className={styles["post__content"]}>
-                <Content date={date} body={html} title={title} />
-            </div>
+      <div className={styles["post__content"]}>
+        <Content date={date} body={html} title={title} />
+      </div>
 
-            <div className={styles["post__footer"]}>
-                {/* <SubscribeBox /> */}
-                {tags && tagSlugs && <Tags tags={tags} tagSlugs={tagSlugs} />}
-            </div>
+      <div className={styles["post__footer"]}>
+        {/* <SubscribeBox /> */}
+        {tags && tagSlugs && <Tags tags={tags} tagSlugs={tagSlugs} />}
+        <Kofi />
+      </div>
 
-            <div className={styles["post__comments"]}>
-                <button className={styles["post__comments__toggle"]} onClick={() => toggleShowComment(!showComment)}>
-                    {showComment ? "Hide comments" : "Show comments"}
-                </button>
+      <div className={styles["post__comments"]}>
+        <button
+          className={styles["post__comments__toggle"]}
+          onClick={() => toggleShowComment(!showComment)}
+        >
+          {showComment ? "Hide comments" : "Show comments"}
+        </button>
 
-                {showComment && <Comments postSlug={slug} postTitle={post.frontmatter.title} fbCommentUrl={fbCommentUrl} />}
-            </div>
-        </div>
-    );
+        {showComment && (
+          <Comments
+            postSlug={slug}
+            postTitle={post.frontmatter.title}
+            fbCommentUrl={fbCommentUrl}
+          />
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Post;
