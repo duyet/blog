ef4bf01d0490cb1b69af3cb77cc21c05e4a2dd48
Duyet Le <lvduyet@fossil.com>
2022-02-13 12:26:14 +0700

    fix: unsplash broken link

--- config.js
+++ config.js
@@ -1,67 +1,71 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  url: 'https://blog.duyet.net',
-  pathPrefix: '/',
-  title: 'Tôi là Duyệt',
-  subtitle: 'Data Engineer @ Fossil. I blog about web development, machine learning, data engineering and more.',
-  copyright: '© duyet.net',
-  disqusShortname: '',
+  url: "https://blog.duyet.net",
+  pathPrefix: "/",
+  title: "Tôi là Duyệt",
+  subtitle:
+    "Data Engineer @ Fossil. I blog about web development, machine learning, data engineering and more.",
+  copyright: "© duyet.net",
+  disqusShortname: "",
   useCommento: true,
   facebookComment: {
     active: true,
     appId: 1394054914200945,
   },
   postsPerPage: 8,
-  googleAnalyticsId: 'UA-92451506-3',
-  googleTagManagerId: 'GTM-PT3ZDS3',
+  googleAnalyticsId: "UA-92451506-3",
+  googleTagManagerId: "GTM-PT3ZDS3",
   useKatex: false,
   menu: [
     {
-      label: 'Blog',
-      path: '/'
+      label: "Blog",
+      path: "/",
     },
     {
-      label: 'About me',
-      path: '/pages/about'
+      label: "About me",
+      path: "/pages/about",
     },
     {
-      label: 'Open Analytics',
-      path: '/pages/open-analytics'
+      label: "Open Analytics",
+      path: "/pages/open-analytics",
     },
     {
-      label: 'Categories',
-      path: '/categories'
+      label: "Categories",
+      path: "/categories",
     },
     {
-      label: 'Unsplash',
-      path: 'https://unsplash.com/@duyetdev'
-    },  
-    {
-      label: 'Buy Me a Coffee',
-      path: 'https://ko-fi.com/duyet'
+      label: "Unsplash",
+      path: "https://unsplash.com/@_duyet",
     },
     {
-      label: 'Today I Learned',
-      path: 'https://til.duyet.net/?utm_source=blog.duyet.net&utm_medium=sidebar_link&utm_campaign=okie_one'
+      label: "Buy Me a Coffee",
+      path: "https://ko-fi.com/duyet",
     },
     {
-      label: 'Feedback.okie.one',
-      path: 'https://feedback.okie.one?utm_source=blog.duyet.net&utm_medium=sidebar_link&utm_campaign=okie_one'
-    }
+      label: "Today I Learned",
+      path:
+        "https://til.duyet.net/?utm_source=blog.duyet.net&utm_medium=sidebar_link&utm_campaign=okie_one",
+    },
+    // {
+    //   label: "Feedback.okie.one",
+    //   path:
+    //     "https://feedback.okie.one?utm_source=blog.duyet.net&utm_medium=sidebar_link&utm_campaign=okie_one",
+    // },
   ],
   author: {
-    name: 'Tôi là Duyệt',
-    photo: '/duyet.jpg',
-    bio: 'Data Engineer @ Fossil. Mostly about Cloud, <a href="/tag/data-engineer/">Data Engineering</a>, <a href="/tag/web/">Web</a>, and <a href="/categories/">more topics</a>.',
+    name: "Tôi là Duyệt",
+    photo: "/duyet.jpg",
+    bio:
+      'Data Engineer @ Fossil. Mostly about Cloud, <a href="/tag/data-engineer/">Data Engineering</a>, <a href="/tag/web/">Web</a>, and <a href="/categories/">more topics</a>.',
     contacts: {
-      email: '',
-      telegram: '',
-      twitter: 'duyetdev',
-      github: 'duyet',
-      rss: '/rss.xml',
-      vkontakte: '',
-      kofi: 'duyet'
-    }
-  }
+      email: "",
+      telegram: "",
+      twitter: "duyetdev",
+      github: "duyet",
+      rss: "/rss.xml",
+      vkontakte: "",
+      kofi: "duyet",
+    },
+  },
 };
