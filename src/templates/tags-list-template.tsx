c43af47ec67316f4843a0904127206e72aa712e7
Duyet Le <lvduyet@fossil.com>
2022-02-16 23:00:31 +0700

    chore: sort tags by count

--- src/templates/tags-list-template.tsx
+++ src/templates/tags-list-template.tsx
@@ -1,22 +1,30 @@
 // @flow strict
-import React from 'react';
-import { Link } from 'gatsby';
-import kebabCase from 'lodash/kebabCase';
-import Layout from '../components/Layout';
-import Sidebar from '../components/Sidebar';
-import Page from '../components/Page';
-import { useSiteMetadata, useTagsList } from '../hooks';
+import React from "react";
+import { Link } from "gatsby";
+import kebabCase from "lodash/kebabCase";
+import Layout from "../components/Layout";
+import Sidebar from "../components/Sidebar";
+import Page from "../components/Page";
+import { useSiteMetadata, useTagsList } from "../hooks";
+
+type TagNode = {
+  fieldValue: string;
+  totalCount: number;
+};
 
 const TagsListTemplate = () => {
   const { title, subtitle } = useSiteMetadata();
   const tags = useTagsList();
+
+  // sort tags by totalCount
+  const sortedTags = tags.sort((a: TagNode, b: TagNode) => b.totalCount - a.totalCount);
 
   return (
     <Layout title={`Tags - ${title}`} description={subtitle}>
       <Sidebar />
       <Page title="Tags">
         <ul>
-          {tags.map((tag: any) => (
+          {sortedTags.map((tag: any) => (
             <li key={tag.fieldValue}>
               <Link to={`/tag/${kebabCase(tag.fieldValue)}/`}>
                 {tag.fieldValue} ({tag.totalCount})
