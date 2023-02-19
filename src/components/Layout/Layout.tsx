560271b56b6068da7d64d3c1ba2cabcfa9bf5ff5
Duyet Le <lvduyet@fossil.com>
2023-02-19 22:10:35 +0700

    chore: add amplitude

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import Helmet from 'react-helmet';
 import type { Node as ReactNode } from 'react';
+import { init } from '@amplitude/analytics-browser';
 import * as styles from './Layout.module.scss';
 
 type Props = {
@@ -9,6 +10,8 @@ type Props = {
   title: string,
   description?: string
 };
+
+init('3a35b45a95aae45c66362440740d4419');
 
 const Layout = ({ children, title, description }: Props) => (
   <div className={styles.layout}>
@@ -21,6 +24,7 @@ const Layout = ({ children, title, description }: Props) => (
       <meta name="twitter:title" content={title} />
       <script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "1fc269287fc7476cacbe4cb0b3267201"}'></script>
       <script async src="https://cdn.splitbee.io/sb.js"></script>
+
     </Helmet>
     {children}
   </div>
