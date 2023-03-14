fcdc73ba67b03453cdbd37baa885bca00a03ec89
Duyet Le <5009534+duyet@users.noreply.github.com>
2023-03-14 14:34:18 +0700

    Update Layout.tsx

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -24,7 +24,7 @@ const Layout = ({ children, title, description }: Props) => (
       <meta name="twitter:title" content={title} />
       <script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "1fc269287fc7476cacbe4cb0b3267201"}'></script>
       <script async src="https://cdn.splitbee.io/sb.js"></script>
-      <script async>
+      <script>
         const script = document.createElement('script');
         script.src = 'https://pageview.duyet.net/api/pageview?url=' + window.location.href;
         document.body.appendChild(script);
