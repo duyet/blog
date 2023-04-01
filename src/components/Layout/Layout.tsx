eafe88cf251fbff82a43088de6328ed0d199ec59
Duyet Le <duyet.cs@gmail.com>
2023-04-02 00:36:30 +0700

    fix: fix pageview

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -25,15 +25,10 @@ const Layout = ({ children, title, description }: Props) => (
       <script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "1fc269287fc7476cacbe4cb0b3267201"}'></script>
       <script async src="https://cdn.splitbee.io/sb.js"></script>
       <script>
-        (() => {
-          const track = () => {
-            let s = document.createElement('script');
-            s.src = 'https://pageview.duyet.net/api/pageview?url=' + window.location.href;
-            document.body.appendChild(s);
-          }
-          window.addEventListener('locationchange', track);
-          track();
-        })()
+        !function(e,n,t){e.onload=function(){
+        let e=n.createElement("script");
+        e.src=t,n.body.appendChild(e)}}
+        (window,document,"//pageview.duyet.net/pageview.js");
       </script>
     </Helmet>
     {children}
